a4ebae7732b19f69b6f9a27c092ec0ebea8ebf8c
Ritesh Tiwary <37486223+ritesh-tiwary@users.noreply.github.com>
2024-12-03 07:36:29 +0000

    Initial commit

--- harvest_hawk_scraper/harvest_hawk_scraper/spiders/phonakshopfinder.py
+++ harvest_hawk_scraper/harvest_hawk_scraper/spiders/phonakshopfinder.py
@@ -4,6 +4,7 @@ import scrapy
 
 class PhonakshopfinderSpider(scrapy.Spider):
     name = "phonakshopfinder"
+    custom_settings = {"ROBOTSTXT_OBEY": False}
     locations = ["Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chhattisgarh", "Goa", "Gujarat", "Haryana", "Himachal Pradesh", "Jharkhand", "Karnataka", "Kerala", "Maharashtra", "Madhya Pradesh", "Manipur", "Meghalaya", "Mizoram", "Nagaland", "Odisha", "Punjab", "Rajasthan", "Sikkim", "Tamil Nadu", "Tripura", "Telangana", "Uttar Pradesh", "Uttarakhand", "West Bengal", "Andaman Nicobar", "Chandigarh", "Dadra Nagar Haveli and Daman Diu", "Delhi", "Jammu Kashmir", "Ladakh", "Lakshadweep", "Puducherry"]
 
 
